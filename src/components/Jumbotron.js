e0e788e1453fc12776dc50d76e9dc706cecf6d5a
twilight-warlock <devansh.ts@somaiya.edu>
2020-08-17 00:21:30 +0530

    Added react-particles-js and added bubbles animated background

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,9 +1,60 @@
 import React, { Fragment } from "react";
+import Particles from "react-particles-js";
 
 const Jumbotron = () => {
   return (
     <Fragment>
       <section className="section-home">
+        <Particles
+          style={{ height: "100vh" }}
+          params={{
+            particles: {
+              number: {
+                value: 160,
+                density: {
+                  enable: false,
+                },
+              },
+              size: {
+                value: 3,
+                random: true,
+                anim: {
+                  speed: 4,
+                  size_min: 0.3,
+                },
+              },
+              line_linked: {
+                enable: false,
+              },
+              move: {
+                random: true,
+                speed: 1,
+                direction: "top",
+                out_mode: "out",
+              },
+            },
+            interactivity: {
+              events: {
+                onhover: {
+                  enable: true,
+                  mode: "bubble",
+                },
+              },
+              modes: {
+                bubble: {
+                  distance: 250,
+                  duration: 2,
+                  size: 0,
+                  opacity: 0,
+                },
+                repulse: {
+                  distance: 400,
+                  duration: 4,
+                },
+              },
+            },
+          }}
+        />
         <div className="box">
           <div className="d-none d-md-block">
             <h1 style={{ lineHeight: 0.9 }} className="white">
